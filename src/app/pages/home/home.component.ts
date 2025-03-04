8fd89f531c3c4bf6b753103baa5e7bbb52de6b37
Christopher DeLeon <christopherdeleon@Chriss-MacBook-Pro.local>
2025-03-04 16:05:09 -0500

    adding model lineup carousel

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,6 +6,18 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
 
+export class HomeComponent {
+  nissanAltimaCarousel = 'assets/nissan-altima-carousel.png';
+  nissanLeafCarousel = 'assets/nissan-leaf-carousel.png';
+  nissanSentraCarousel = 'assets/nissan-sentra-carousel.png';
+  nissanVersaCarousel = 'assets/nissan-versa-carousel.png';
+  nissanAriyaCarousel = 'assets/nissan-ariya-carousel.png';
+  nissanZCarousel = 'assets/nissan-z-carousel.png';
+  nissanArmadaCarousel = 'assets/nissan-armada-carousel.png';
+  nissanFrontierCarousel = 'assets/nissan-frontier-carousel.png';
+  nissanKicksCarousel = 'assets/nissan-kicks-carousel.png';
+  nissanRogueCarousel = 'assets/nissan-rogue-carousel.png';
+  nissanPathfinderCarousel = 'assets/nissan-pathfinder-carousel.png';
+  nissanMuranoCarousel = 'assets/nissan-murano-carousel.png';
 }
